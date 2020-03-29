f0545899566840b1102f8c0447da1e9b46ca52d8
miteshkumar77 <miteshkumarca@gmail.com>
2020-03-29 17:24:14 -0400

    Scheduled dose deletions work. Now need to send notifications

--- application/server/isHourlyNotificationEligible.js
+++ application/server/isHourlyNotificationEligible.js
@@ -11,24 +11,84 @@ async function hourlyEligibleNotifications() {
     const today = dayOfWeeks[d.getDay()];
     const hour =  d.getHours();
     const medicineIds = (await db_days.findById({ _id: today })).medicineIds;
-    const meds_db = await Promise.all(
+    const meds_db = Promise.all(
         medicineIds.map(medicineId => {
             return db_meds.find({ _id: medicineId , times: hour });
         })
     );
+    
     return meds_db; 
 }
 
 function sendNotification(meds_db) {
     console.log(meds_db);
+    
 }
 
+async function configureNotification(meds_db) {
 
+    
+    const filtered = meds_db.filter(item => item.length != 0); 
+    if (filtered.length != 0) {
+        console.log(filtered);
 
+        filtered.forEach(async med_arr => {
+            const med = med_arr[0];
+            console.log(med._id); 
+            let res = await db_meds.findByIdAndUpdate(
+                med._id,
+                { $inc: { count: -med.dose }},
+                { new: true, useFindAndModify: false }
+            ).catch((err) => console.error(err));
+
+            if (res.count <= 0) {
+                res = null;
+                await Promise.all([db_meds.findByIdAndDelete(med._id)
+                    .catch((err) => console.error(err)), 
+                    Promise.all(
+                        med.dayNames.map(dayName => {
+                            return db_days.findByIdAndUpdate(dayName, { $pull : { medicineIds: med._id }})
+                            .catch((err) => console.error(err));
+                        })
+                    ).catch((err) => console.error(err))]
+                ).catch((err) => console.error(err));
+            }
+        })
+        
+    }
+}
+
+function executeFuncEveryMinute(func) { // For testing only
+    const d = new Date(); 
+    const sec = d.getSeconds(); 
+    const msUntilNextMinute = (60 - Number.parseInt(sec)) * 1000;
+    console.log(`There are ${msUntilNextMinute / 1000}s until the top of the minute`);
+    setTimeout(() => {
+      func();
+      setInterval(func, 60000);
+    }, 
+    msUntilNextMinute);
+}
+
+function executeFuncEveryHour(func) {
+    const d = new Date(); 
+    const sec = d.getSeconds(); 
+    const hr = d.getHours();
+    const msUntilNextHour = ((60 - Number.parseInt(sec)) + (60 - Number.parseInt(hr)) * 60)*1000
+    console.log(`There are ${msUntilNextHour / 1000}s until the top of the hour`);
+    setTimeout(() => {
+        func();
+        setInterval(func, 3600000);
+    },
+    msUntilNextHour);
+}
 
 
 
 module.exports = {
     hourlyEligibleNotifications,
-    sendNotification
+    sendNotification,
+    configureNotification,
+    executeFuncEveryMinute,
+    executeFuncEveryHour
 };
